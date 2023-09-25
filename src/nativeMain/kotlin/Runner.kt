117eb0cedc3d7a8ef6aef6063562eafdf305bd13
René Jörg Spies <121790537+rjspies@users.noreply.github.com>
2023-09-25 19:04:04 +0200

    Simplify

--- src/nativeMain/kotlin/Runner.kt
+++ src/nativeMain/kotlin/Runner.kt
@@ -4,11 +4,6 @@ import kotlin.time.toDuration
 
 class Runner(private val task: String) {
     fun performRun() {
-        performWarmupRun()
-        performProfiling()
-    }
-
-    private fun performProfiling() {
         println("Performing full run...")
         val durations = mutableListOf<Long>()
         for (count in 0..80) {
@@ -19,10 +14,5 @@ class Runner(private val task: String) {
             durations += duration.toLong(DurationUnit.MILLISECONDS)
         }
         println("Average = ${durations.average().toDuration(DurationUnit.MILLISECONDS)}")
-    }
-
-    private fun performWarmupRun() {
-        println("Performing warmup run...")
-        Gradlew(task).execute()
     }
 }
